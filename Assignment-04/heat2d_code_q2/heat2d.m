11bf8b462da1304d17f7288ff59a08a70ec874ba
2018uch0002 <parth.singh.chemical@gmail.com>
2025-09-28 02:26:34 -0400

    q02 is completed.

--- Assignment-04/heat2d_code_q2/heat2d.m
+++
@@ -1,6 +0,0 @@
-% File written by heat2d.C
-xa=0; xb=1; ya=0; yb=1; kappa=0.1; t=0.5; maxErr= 1.332e-03; cpuTimeStep= 6.837e-01;
-n1a=0; n1b=200; nd1a=-1; nd1b=201;
-n2a=0; n2b=200; nd2a=-1; nd2b=201;
-dx(1)=  5.000000e-03; dx(2)=  5.000000e-03; numGhost=1;
-option=3; optionName='fortranRoutine';
