31772972b2ff3a68170d194d298d142f5643a3e0
2018uch0002 <parth.singh.chemical@gmail.com>
2025-10-13 17:35:41 -0400

    heatADI is completed

---
+++ Assignment-06/heatADI_openmp/tridiagonal.h
@@ -0,0 +1,16 @@
+#ifndef TRIDIAGONAL_H
+#define TRIDIAGONAL_H "tridiagonal.h"
+
+//
+// Routines for solving tridiagonal linear systems
+//
+#include "A++.h"
+
+typedef double Real;
+typedef doubleSerialArray RealArray;
+
+int factorTridiagonalMatrix( RealArray & Ax );
+
+int solveTridiagonal( RealArray & Ax, RealArray & rhs );
+
+#endif
